00b2310b4c29faa594105f18fbd7f43273b71fb0
TuGatita777 <maxi.gonzalezg@duocuc.cl>
2023-11-20 11:00:47 -0300

    act 0.2

--- src/app/pages/cambiar-contrasena-seg/cambiar-contrasena-seg.page.spec.ts
+++ src/app/pages/cambiar-contrasena-seg/cambiar-contrasena-seg.page.spec.ts
@@ -1,15 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 import { CambiarContrasenaSegPage } from './cambiar-contrasena-seg.page';
 
 describe('CambiarContrasenaSegPage', () => {
   let component: CambiarContrasenaSegPage;
   let fixture: ComponentFixture<CambiarContrasenaSegPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async() => {
+    TestBed.configureTestingModule({
+      providers: [SQLite]
+    }).compileComponents();
     fixture = TestBed.createComponent(CambiarContrasenaSegPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/pages/codigo/codigo.page.spec.ts
+++ src/app/pages/codigo/codigo.page.spec.ts
@@ -1,15 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 import { CodigoPage } from './codigo.page';
 
 describe('CodigoPage', () => {
   let component: CodigoPage;
   let fixture: ComponentFixture<CodigoPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async() => {
+    TestBed.configureTestingModule({
+      providers: [SQLite]
+    }).compileComponents();
     fixture = TestBed.createComponent(CodigoPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
